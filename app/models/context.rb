f1955dbbff38de1a7894657280979ddbffdc913d
Ludovic AUGE <auge.ludovic@gmail.com>
2026-03-11 20:56:12 +0100

    fix dependent in models

--- app/models/context.rb
+++ app/models/context.rb
@@ -1,6 +1,6 @@
 class Context < ApplicationRecord
   belongs_to :user
-  has_many :chats, depend: :destroy
+  has_many :chats, dependent: :destroy
 
   has_one_attached :document
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :contexts, depend: :destroy
+  has_many :contexts, dependent: :destroy
 end
