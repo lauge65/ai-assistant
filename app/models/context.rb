44efa674bb082aafd170ce937c3b680058f94995
Ludovic AUGE <auge.ludovic@gmail.com>
2026-03-12 19:03:37 +0100

    action new et create done avec deux contexts crées

--- app/models/context.rb
+++ app/models/context.rb
@@ -3,4 +3,15 @@ class Context < ApplicationRecord
   has_many :chats, dependent: :destroy
 
   has_one_attached :document
+  validates :title, :level, :subject, :date, presence: true
+  validate :document_must_be_attached
+
+  private
+
+  def document_must_be_attached
+    unless document.attached?
+      errors.add(:document, "must be attached")
+    end
+  end
+
 end
