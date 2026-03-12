a7f28740e326c131eaf7fc61bf209bbd753530e0
Ludovic AUGE <auge.ludovic@gmail.com>
2026-03-12 17:51:02 +0100

    Mise en place des routes et du controller contexts

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
-  resources :subjects do
-    resources :contexts, only: [:new, :create]
+  resources :contexts do
+    resources :chats, only: [:show, :create]
   end
 end
