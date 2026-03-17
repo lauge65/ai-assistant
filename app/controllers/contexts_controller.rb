092dbb3fb5b13303dff190a87ee1ff6498b7787c
lucas madec <lucasmadec56@gmail.com>
2026-03-17 18:31:17 +0100

    Contexts_controller show edit update destroy

--- app/controllers/contexts_controller.rb
+++ app/controllers/contexts_controller.rb
@@ -1,28 +1,37 @@
 class ContextsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_context, only: [:edit, :update]
+  before_action :set_context, only: [:show, :edit, :update, :destroy]
 
   def index
     @contexts = current_user.contexts
-  end
-
-  def show
-    @context = current_user.contexts.find(params[:id])
-  end
-
-  def new
     @context = Context.new
   end
 
- def create
-  @context =current_user.contexts.new(context_params)
-  if @context.save
-    redirect_to context_path(@context)
-  else
-    render :new, status: :unprocessable_entity
+  def create
+    @context =current_user.contexts.new(context_params)
+    if @context.save
+      redirect_to context_path(@context)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+  def edit
+     @context
   end
 
- end
+  def update
+    if @context.update(context_params)
+      redirect_to context_path(@context)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @context = current_user.contexts.find(params[:id])
+    @context.destroy
+    redirect_to contexts_path
+  end
 
   private
 
